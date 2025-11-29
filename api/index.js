a71f984049fff1df595165c62f67822a9a08389a
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-29 09:20:36 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+//2sMNL69MQtrOHqfD
+
+// mongodb+srv://Nimas:2sMNL69MQtrOHqfD@mern-estate.kaee86t.mongodb.net/?appName=mern-estate
