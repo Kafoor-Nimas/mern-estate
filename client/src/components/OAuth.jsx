9732f0b5c052b6f6dbc44aa143f0b3da7d4bfe82
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-01 11:52:11 +0530

    create and add OAuth component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,9 +2,11 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -24,6 +26,7 @@ const OAuth = () => {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
       console.log("Could not sign in with google", error);
     }
