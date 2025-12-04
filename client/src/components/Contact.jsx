5436ea59b8d40a88d560038294e58ded9cd0b338
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-04 17:42:09 +0530

    Add contact landlord functionality to the listing page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Contact = ({listing}) => {
+const Contact = ({ listing }) => {
+  const [landlord, setLandLord] = useState(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchLandlord = async () => {
+      try {
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        setLandLord(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLandlord();
+  }, [listing.userRef]);
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+  console.log(message);
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={handleChange}
+            placeholder="Enter your message here..."
+            className="bg-white w-full p-3 rounded-lg"
+          ></textarea>
 
-export default Contact
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 rounded-lg uppercase hover:opacity-90"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Contact;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -116,23 +116,23 @@ const Listing = () => {
             </p>
             <ul className="text-green-900 font-semibold text-sm flex items-center gap-4 sm:gap-6 flex-wrap">
               <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaBed classname="text-lg" />
+                <FaBed className="text-lg" />
                 {listing.bedrooms > 1
                   ? `${listing.bedrooms} beds`
                   : `${listing.bedrooms} bed`}
               </li>
               <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaBath classname="text-lg" />
+                <FaBath className="text-lg" />
                 {listing.bathrooms > 1
                   ? `${listing.bathrooms} baths`
                   : `${listing.bathrooms} bath`}
               </li>
               <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaParking classname="text-lg" />
+                <FaParking className="text-lg" />
                 {listing.parking ? "Parking spot" : "No Parking"}
               </li>
               <li className="flex items-center gap-1 whitespace-nowrap">
-                <FaChair classname="text-lg" />
+                <FaChair className="text-lg" />
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
