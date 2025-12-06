f252e3eac034311698ba4bf5cd8fcbebf2f044f0
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-06 14:14:16 +0530

    finish home top section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      {/* top */}
+      <div className="flex flex-col gap-6 py-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Nimas Estate will help you find your home fast, easy and comfortable.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+        >
+          Let's get started...
+        </Link>
+      </div>
+      {/* swiper */}
 
-export default Home
+      {/* listing results for offer, sale and rent */}
+    </div>
+  );
+};
+
+export default Home;
