fdadeadc63a7fe976a4d3060b1d6362a9a82bff9
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-06 17:57:38 +0530

    add pre installation for deployement

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 import uploadRouter from "./routes/upload.route.js";
+import path from "path";
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -16,6 +17,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -29,6 +32,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/upload", uploadRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //create a middleware for error handling
 app.use((err, req, res, next) => {
